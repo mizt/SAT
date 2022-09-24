6ad87333dbe010262f6c69f3dce4711f3ce1c7b5
mizt <info@mizt.org>
2022-09-24 11:55:28 +0900

    Update

--- main.mm
+++ main.mm
@@ -16,11 +16,12 @@ void sumX(unsigned int *sum, unsigned int *src, int w, int h, int begin, int end
 	unsigned int bgr[BGR] = {0,0,0};
 	for(int i=begin; i<end; i++) {
 		for(int n=0; n<BGR; n++) bgr[n]=0;
+		unsigned int *pSum = sum+i*w*BGR;
+		unsigned int *pSrc = src+i*w;
 		for(int j=0; j<w; j++) {
-			unsigned int pixel = src[i*w+j];
-			unsigned int addr = (i*w+j)*BGR;
+			unsigned int pixel = *pSrc++;
 			for(int n=0; n<BGR; n++) {
-				sum[addr++] = (bgr[n]+=((pixel>>(0x10-(n<<3)))&0xFF));
+				*pSum++=(bgr[n]+=(pixel>>(0x10-(n<<3)))&0xFF);
 			}
 		}
 	}
@@ -37,11 +38,9 @@ void sumX(unsigned int *sum, unsigned int *src, int w, int h) {
 	dispatch_group_async(_group,_queue,^{
 		sumX(sum,src,w,h,(h>>2)*1,(h>>2)*2);
 	});
-	
 	dispatch_group_async(_group,_queue,^{
 		sumX(sum,src,w,h,(h>>2)*2,(h>>2)*3);
 	});
-	
 	dispatch_group_async(_group,_queue,^{
 		sumX(sum,src,w,h,(h>>2)*3,h);
 	});
@@ -56,14 +55,11 @@ void sumY(unsigned int *sum, int w, int h, int begin, int end) {
 		for(int i=0; i<h; i++) {
 			unsigned int addr = (i*w+j)*BGR;
 			for(int n=0; n<BGR; n++) {
-				bgr[n]+=sum[addr];
-				sum[addr] = bgr[n];
-				addr++;
+				sum[addr++]=(bgr[n]+=sum[addr]);
 			}
 		}
 	}
 }
-
 
 void sumY(unsigned int *sum, int w, int h) {
 	
@@ -86,51 +82,63 @@ void sumY(unsigned int *sum, int w, int h) {
 	dispatch_group_wait(_group,DISPATCH_TIME_FOREVER);
 }
 
-void blur(unsigned int *dst, unsigned int *src, unsigned int *sum, unsigned short *radius, int w, int h, int begin, int end) {
+static void blur(unsigned char *bgr, unsigned int *sum, int w, int h, int j, int i, int r) {
+	
+	int top = i-r;
+	int bottom=i+r;
+	int left=j-r;
+	int right=j+r;
+	
+	if(top<0) top = 0;
+	if(bottom>h-1) bottom = h-1;
+	
+	int y = (bottom-top);
+
+	if(left<0) left = 0;
+	if(right>w-1) right = w-1; 
+	int x = (right-left);
+	
+	double area = 1.0/(double)(x*y);
+	
+	unsigned int TL = (top*w+left)*BGR;
+	unsigned int TR = (top*w+right)*BGR;
+	unsigned int BL = (bottom*w+left)*BGR;
+	unsigned int BR = (bottom*w+right)*BGR;
+	
+	bgr[0] = CLAMP255((sum[BR]-sum[BL]-sum[TR]+sum[TL])*area);
+	bgr[1] = CLAMP255((sum[BR+1]-sum[BL+1]-sum[TR+1]+sum[TL+1])*area);
+	bgr[2] = CLAMP255((sum[BR+2]-sum[BL+2]-sum[TR+2]+sum[TL+2])*area);
+}
+
+void blur(unsigned int *dst, unsigned int *src, unsigned int *sum, unsigned int *radius, int w, int h, int begin, int end) {
+
+	unsigned char bgr[3] = {0,0,0};
 
 	for(int i=begin; i<end; i++) {
 		for(int j=0; j<w; j++) {
 			
-			unsigned int addr = (i*w+j) ;
+			unsigned int addr = i*w+j;
 			
-			if(radius[addr]==0) {
-				dst[i*w+j] = src[i*w+j];
+			unsigned int r = radius[addr];
+			
+			if(r==0) {
+				dst[addr] = src[addr];
 			}
 			else {
 				
-				int top = i-radius[addr];
-				if(top<0) top = 0;
+				blur(bgr,sum,w,h,j,i,r);
 				
-				int bottom = i+radius[addr];
-				if(bottom>h-1) bottom = h-1;
+				unsigned char blue = bgr[0]; 
+				unsigned char green = bgr[1]; 
+				unsigned char red = bgr[2]; 
 				
-				int y = (bottom-top);
-				
-				int left = j-radius[addr];
-				if(left<0) left = 0;
-				
-				int right = j+radius[addr];
-				if(right>w-1) right = w-1; 
-				int x = (right-left);
-				
-				double area = 1.0/(double)(x*y);
-				
-				int TL = (top*w+left)*BGR;
-				int TR = (top*w+right)*BGR;
-				int BL = (bottom*w+left)*BGR;
-				int BR = (bottom*w+right)*BGR;
-				
-				unsigned int b = (sum[BR+0]-sum[BL+0]-sum[TR+0]+sum[TL+0])*area;
-				unsigned int g = (sum[BR+1]-sum[BL+1]-sum[TR+1]+sum[TL+1])*area;
-				unsigned int r = (sum[BR+2]-sum[BL+2]-sum[TR+2]+sum[TL+2])*area;
-				
-				dst[i*w+j] = 0xFF000000|CLAMP255(b)<<16|CLAMP255(g)<<8|CLAMP255(r);
+				dst[i*w+j] = 0xFF000000|blue<<16|green<<8|red;
 			}
 		}
 	}
 }
 
-void blur(unsigned int *dst, unsigned int *src, unsigned int *sum, unsigned short *radius, int w, int h) {
+void blur(unsigned int *dst, unsigned int *src, unsigned int *sum, unsigned int *radius, int w, int h) {
 	
 	dispatch_group_t _group = dispatch_group_create();
 	dispatch_queue_t _queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH,0);
@@ -171,7 +179,7 @@ int main(int argc, char *argv[]) {
 			unsigned int *sum = new unsigned int[w*h*BGR];
 			unsigned int *depth = (unsigned int *)stb_image::stbi_load("./depth.png",info,info+1,info+2,4);
 
-			unsigned short *radius = new unsigned short[w*h];
+			unsigned int *radius = new unsigned int[w*h];
 			for(int i=0; i<h; i++) {
 				for(int j=0; j<w; j++) {
 					unsigned int addr = (i*w+j);
@@ -184,8 +192,8 @@ int main(int argc, char *argv[]) {
 			}
 			
 			int bgr[BGR] = {0,0,0};
-			
-double then = CFAbsoluteTimeGetCurrent();
+
+			double then = CFAbsoluteTimeGetCurrent();
 			
 			sumX(sum,src,w,h);
 			sumY(sum,w,h);
@@ -196,7 +204,6 @@ double then = CFAbsoluteTimeGetCurrent();
 			blur(dst,buf,sum,radius,w,h);
 
 NSLog(@"%f",CFAbsoluteTimeGetCurrent()-then);
-			
 			
 			stb_image::stbi_write_png("./blur.png",w,h,4,(void const*)dst,w<<2);
 			
